eb04aff6df787c94a9852fa194cc9eacce63421f
Hiroaki Takisawa <takisawa@hit-start.jp>
2017-10-05 22:41:47 +0900

    Add recipe for install rbenv.

--- cookbooks/rbenv/default.rb
+++ cookbooks/rbenv/default.rb
@@ -22,3 +22,15 @@ git '/home/deploy/.rbenv/plugins/ruby-build' do
   repository 'https://github.com/sstephenson/ruby-build.git'
   user 'deploy'
 end
+
+remote_file '/home/deploy/.bash_profile_rbenv' do
+  owner 'deploy'
+  group 'deploy'
+  mode '0644'
+end
+
+execute 'install ruby 2.4.2' do
+  command 'source /home/deploy/.bash_profile_rbenv; rbenv install 2.4.2'
+  not_if 'source /home/deploy/.bash_profile_rbenv; rbenv versions | grep 2.4.2'
+  user 'deploy'
+end
